1ad06110b7e89fe82eee1e4f1acd3906cc1c429a
叶德文 <675573096@qq.com>
2021-08-01 00:47:58 +0800

    init

---
+++ deploy-gh.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
+git push -f git@github.com:ydw233/blog.git master:gh-pages
+
+cd -
+
+作者：Mark同学104391
+链接：https://juejin.cn/post/6844904122873806856
+来源：掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+# Build Setup
+
+### install dependency
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev 
+```
